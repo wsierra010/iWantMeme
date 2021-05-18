046fa124ca786d706f3ca0672463fdbb588e1397
wsierra010 <wsierra010@gmail.com>
2021-05-18 15:13:58 +0200

    Create server with nodejs

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const expresshbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+app.set('port', process.env.PORT || 4000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+    ".hbs",
+    expresshbs({
+        defaultLayout: "main",
+        layoutsDir: path.join(app.get("views"), "layouts"),
+        partialsDir: path.join(app.get("views"), "partials"),
+        extname: ".hbs",
+        helpers: require("./lib/handlebars"),
+    })
+);
+app.set('view engine', 'hbs');
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Global variables
+app.use((req, res, next) => {
+
+    next();
+});
+// Routes
+app.use(require('./routes/index'));
+app.use(require('./routes/auth'));
+
+// Public
+app.use(express.static(path.join(__dirname, 'public')));
+// Starting the server
+app.listen(app.get("port"), () => {
+    console.log("Server is in port", app.get("port"));
+});
